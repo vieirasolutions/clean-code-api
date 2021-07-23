5b87349bf6566e0a094d18631e16eb2907da110b
Breno Vieira Soares <breno@lifenpag.com.br>
2021-07-22 23:58:54 -0300

    test: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,6 @@ describe('SignUp Controller', () => {
     }
     const httpReponse = sut.handle(httpRequest)
     expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.body).toEqual(new Error('Missing param: name'))
   })
 })
