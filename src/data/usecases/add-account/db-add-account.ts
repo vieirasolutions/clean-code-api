5886d8539e42134dca0e3d9d929c797ca5ff5644
Breno Vieira Soares <breno@lifenpag.com.br>
2021-07-24 21:48:18 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -12,7 +12,7 @@ export class DbAddAccount implements AddAccount {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const { password } = accountData
     const hashedPassword = await this.encrypter.encrypt(password)
-    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
     return account
   }
 }
