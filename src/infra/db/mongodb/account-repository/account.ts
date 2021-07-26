643a07bf2a3da74ad9f22c92be591a9259e3cfd4
Breno Vieira Soares <breno@lifenpag.com.br>
2021-07-25 21:25:23 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,18 +2,11 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
-
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = await accountCollection.findOne(result.insertedId)
-    if (!account) {
-      throw new Error('Account not found')
-    }
-
-    const { _id, ...accountWithoutId } = account
-
-    return (Object.assign({}, accountWithoutId, { id: _id }) as AccountModel)
+    return MongoHelper.map(account)
   }
 }
