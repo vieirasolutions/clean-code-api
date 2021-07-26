0a1dc6a52a82fc871a328be415eddaa980b6c0ac
Breno Vieira Soares <breno@lifenpag.com.br>
2021-07-25 21:07:08 -0300

    feat: ensure AccountMongoRepository returns an account os success

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ export default {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
