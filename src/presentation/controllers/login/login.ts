1e63706bc4762763ee758f82afea4f6bc3f792f0
Breno Vieira Soares <brenovieirasoares1999@gmail.com>
2021-07-26 22:18:14 -0300

    feat: ensure LoginController returns ok if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, EmailValidator, HttpRequest, HttpResponse, Authentication } from './login-protocols'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -30,6 +30,8 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
