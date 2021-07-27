b8ceef8c887f6c316cfd4f9ae6a05df5fc1fbaab
Breno Vieira Soares <brenovieirasoares1999@gmail.com>
2021-07-27 12:54:10 -0300

    test: ensure SignupController return a bad request if Validation returns an error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -162,4 +162,11 @@ describe('SignUp Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('Should return bad request if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpReponse = await sut.handle(makeFakeRequest())
+    expect(httpReponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
