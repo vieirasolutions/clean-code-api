390031d2f6102a1082bdfab01b6b8188b3d6ccc0
Breno Vieira Soares <brenovieirasoares1999@gmail.com>
2021-07-30 19:02:07 -0300

    feat: ensure AddSurveyController calss Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,13 @@
+import { Validation } from '../../../protocols'
+import { Controller, HttpRequest, HttpResponse } from './add-survey-controller-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
+    return await new Promise(resolve => resolve(null))
+  }
+}
