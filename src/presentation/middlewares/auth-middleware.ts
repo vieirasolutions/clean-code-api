d1f04107571c640d809f66688a416fb0dd77b57d
Breno Vieira Soares <brenovieirasoares1999@gmail.com>
2021-08-02 16:09:04 -0300

    feat: ensure AuthMiddleware returns serverError if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { UnauthenticatedError } from '../errors'
-import { forbidden, ok } from '../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -9,16 +9,20 @@ export class AuthMiddleware implements Middleware {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (!accessToken) {
-      return forbidden(new UnauthenticatedError())
-    }
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (!accessToken) {
+        return forbidden(new UnauthenticatedError())
+      }
 
-    const account = await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
-    if (!account) {
-      return forbidden(new UnauthenticatedError())
-    }
+      const account = await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
+      if (!account) {
+        return forbidden(new UnauthenticatedError())
+      }
 
-    return ok({ accountId: account.id })
+      return ok({ accountId: account.id })
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
