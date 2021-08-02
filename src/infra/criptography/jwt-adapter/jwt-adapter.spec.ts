b1387026dccff43a8febcf6116ec01c51f6ecfef
Breno Vieira Soares <brenovieirasoares1999@gmail.com>
2021-08-02 17:18:35 -0300

    test: ensure JwtAdapter throw if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -51,10 +51,17 @@ describe('Jwt Adapter', () => {
       expect(verifySpy).toBeCalledWith('any_token', 'secret')
     })
 
-    test('Should return a value on sign success', async () => {
+    test('Should return a value on verify success', async () => {
       const { sut } = makeSut()
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
+    })
+
+    test('Should throw if verify throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
